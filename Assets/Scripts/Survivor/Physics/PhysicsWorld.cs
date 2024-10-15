21f6a665203417ebf90f3baafc36618ca2edf32c
tianwen <yaohii@foxmail.com>
2024-10-15 21:45:54 +0800

    Update PhysicsWorld.cs

--- Assets/Scripts/Survivor/Physics/PhysicsWorld.cs
+++ Assets/Scripts/Survivor/Physics/PhysicsWorld.cs
@@ -9,8 +9,7 @@ namespace Survivor.Physics
     public partial class PhysicsWorld
     {
         public QuadTree<entlong> tree;
-        public void Step(){}
-        
+        public void Step(float deltaTime){}
         
         
     }
@@ -18,6 +17,12 @@ namespace Survivor.Physics
     {
         public float halfWidth;
         public float halfHeight;
+
+        public Bounds(float halfWidth, float halfHeight)
+        {
+            this.halfWidth = halfWidth;
+            this.halfHeight = halfHeight;
+        }
     }
     
     [Flags]
@@ -119,20 +124,101 @@ namespace Survivor.Physics
     /// <typeparam name="T"></typeparam>
     public class QuadTree<T>
     {
+        private int _maxLayer;
+        private Bounds _bounds;
+        private Node _root;
+        private NodePool<Node> _nodePool;
+        
         //初始化根节点
+        public void Init(int maxLayer,Bounds bounds)
+        {
+            _maxLayer = maxLayer;
+            _bounds = bounds;
+            _root = _nodePool.PopNode();
+            _root.Init(0,this,_bounds);
+            _nodePool = new NodePool<Node>(()=>new Node());
+        }
         //获取节点
+        public void GetNode(){}
         //插入节点
+        public void Insert(T data)
+        {
+            
+        }
+
+        
         //更新
+        public void Update(){}
+        public class Node:INodePool
+        {
+            public QuadTree<T> belongTree;
+
+            public int layer;
+            //边界
+            public Bounds bounds;
+            //松散边界
+            public Bounds looseBounds;
+            public Node father;
+            public List<Node> childList = new List<Node>(4);
+            public List<T> nodeDataList = new();
+
+            public void Init(int layer,QuadTree<T> belongTree,Bounds bounds)
+            {
+                this.layer = layer;
+                this.belongTree = belongTree;
+                this.bounds = bounds;
+                looseBounds = new Bounds(bounds.halfWidth *2,bounds.halfHeight*2);
+                
+            }
+
+            void INodePool.OnSpawn()
+            {
+                
+            }
+            void INodePool.OnRelease()
+            {
+            
+            }
+        
+            public void Insert(T e,int maxLayer,int splitThreshold){}
+            public void Merge()
+            {
+                //将子节点的item添加到这个结点中，子节点push到结点池
+            
+            }
+        }
+
+        public class NodePool<TK> where TK:INodePool
+        {
+            private Stack<TK> _nodes = new();
+            private Func<TK> _createFunc;
+
+            public NodePool(Func<TK> createNodeFunc)
+            {
+                _createFunc = createNodeFunc;
+            }
+
+            public void PushNode(TK node)
+            {
+                _nodes.Push(node);
+                node.OnRelease();
+            }
+
+            public TK PopNode()
+            {
+                TK node = _nodes.Count > 0 ? _nodes.Pop() : _createFunc.Invoke();
+                node.OnSpawn();
+                return node;
+            }
+        }
+        
+        public interface INodePool
+        {
+            void OnSpawn();
+            void OnRelease();
+        }
     }
 
-    public class QuadTreeNode<T>
-    {
-        public int layer;
-        public QuadTree<T> belongTree;
-        public Bounds bounds;
-        public QuadTreeNode<T> father;
-        public List<T> nodeDataList;
-    }
     
     
     
